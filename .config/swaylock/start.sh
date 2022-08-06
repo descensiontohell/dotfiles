423f84ab88eefff444e90d155fa156ca6d118826
Ilya Sukhanenkov <isuhanen@gmail.com>
2022-08-06 21:07:43 +0300

    add swaylock

---
+++ .config/swaylock/start.sh
@@ -0,0 +1,29 @@
+swaylock \
+  -S -K \
+  --clock \
+  --indicator \
+  --indicator-radius 200\
+  --indicator-thickness 7\
+  --effect-blur 5x3\
+  --ring-color aaaaaa00\
+  --ring-ver-color aaaaaa00\
+  --ring-clear-color aaaaaa\
+  --ring-ver-color aaaaaa\
+  --key-hl-color a4b6ff\
+  --line-color ffffff00\
+  --line-clear-color ffffff00\
+  --line-ver-color ffffff00\
+  --line-wrong-color ffffff00\
+  --inside-color ffffff00\
+  --inside-clear-color ffffff00\
+  --inside-ver-color ffffff00\
+  --inside-color ffffff00\
+  --text-color ffffff\
+  --text-clear-color dde5ed\
+  --text-ver-color dde5ed\
+  --layout-text-color ffffff\
+  --separator-color ffffff\
+  --grace 0\
+  --fade-in 0.2\
+
+
